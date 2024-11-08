5d8dd187dacc193d9aa1adc8a485a7f1a7d2111d
Phai <phai.339@gmail.com>
2024-11-08 11:00:27 +0700

    u

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,9 @@ function App() {
   return (
     <>
     <LoginSingup />
-      <h1>hello</h1>
-
+      <h1 className='text-amber-50 bg-orange-500'>hello</h1>
+      <h1 className="text-3xl font-bold underline">
+    Hello world! </h1>
     </>
   )
 }
--- src/component/LoginSignup/LoginSingup.jsx
+++ src/component/LoginSignup/LoginSingup.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function LoginSingup() {
   return (
     <div>
-        sdfsdfsdfsdf
+   
       <img src="src/assets/img/user (1).png" alt="" />
     </div>
   )
