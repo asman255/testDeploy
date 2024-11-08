5d4308afcf8138a1f324238cf64be2742d6ba0a9
asman255 <smandth@gmail.com>
2024-11-08 11:52:50 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,15 @@ function App() {
       <h1 className="text-3xl font-bold underline text-red-700">
         Hello world!
       </h1>
+      <h1 className="text-3xl font-bold underline text-red-700">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold underline text-red-700">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold underline text-red-700">
+        Hello world!
+      </h1>
       
       
     </>
