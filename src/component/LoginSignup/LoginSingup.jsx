fca3ff0f2a4cf76e637530282351e7e48ce52f9b
Phai <phai.339@gmail.com>
2024-11-08 10:44:14 +0700

    update

---
+++ src/component/LoginSignup/LoginSingup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function LoginSingup() {
+  return (
+    <div>
+        sdfsdfsdfsdf
+      <img src="src/assets/img/user (1).png" alt="" />
+    </div>
+  )
+}
