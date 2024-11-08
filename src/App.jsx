2e2cf197c9120d8a6a484a711a5c52f7510b11b3
Phai <phai.339@gmail.com>
2024-11-08 10:50:30 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LoginSingup from './component/LoginSignup/LoginSingup'
+import LoginSingup from "./component/LoginSignup/LoginSingup"
 function App() {
   const [count, setCount] = useState(0)
 
