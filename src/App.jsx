addf35364bbde9f02c52b9a202191a8ff9e4a300
asman255 <smandth@gmail.com>
2024-11-11 13:55:59 +0700

    add noto font as default

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,20 @@ function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline bg-slate-700 ">
-    Hello world!YYYYYYYY
-  </h1>
-       
+
+      <div className="p-8">
+        <h1 className="text-3xl font-thin mb-4">All Category หมวดหมู่ทั้งหมด 0123456789 (Thin)</h1>
+        <h1 className="text-3xl font-extralight mb-4">All Category หมวดหมู่ทั้งหมด 0123456789 (Extra Light)</h1>
+        <h1 className="text-3xl font-light mb-4">All Category หมวดหมู่ทั้งหมด 0123456789 (Light)</h1>
+        <h1 className="text-3xl font-normal mb-4">All Category หมวดหมู่ทั้งหมด 0123456789 (Normal)</h1>
+        <h1 className="text-3xl font-medium mb-4">All Category หมวดหมู่ทั้งหมด 0123456789 (Medium)</h1>
+        <h1 className="text-3xl font-semibold mb-4">All Category หมวดหมู่ทั้งหมด 0123456789 (Semi Bold)</h1>
+        <h1 className="text-3xl font-bold mb-4">All Category หมวดหมู่ทั้งหมด 0123456789 (Bold)</h1>
+        <h1 className="text-3xl font-extrabold mb-4">All Category หมวดหมู่ทั้งหมด 0123456789 (Extra Bold)</h1>
+        <h1 className="text-3xl font-black mb-4">All Category หมวดหมู่ทั้งหมด 0123456789 (Black)</h1>
+      </div>
+
+
     </>
   )
 }
