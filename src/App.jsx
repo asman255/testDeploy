18e750d5ad69acc118bd963f78ba012c289ec771
Phai <phai.339@gmail.com>
2024-11-11 16:22:57 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Category from './page/Category'
-
+import Tag from "./components/Tag/Tag"
 function App() {
 
 
   return (
     <>
     <div>
-    <Category />
+  
     </div>
-   
+   <Tag />
     </>
   )
 }
--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,1 +1,18 @@
+import React from 'react'
+
+
+
+export default function Tag() {
+
+  return (
+    <div className='flex'>
+      <p className='text-ga-primary  w-24  
+      text-center ml-3 p-2 rounded-3xl border-2 border-solid border-ga-primary
+      ' >#ปวดหัว</p>
+          <p className='text-ga-primary  w-24 
+      text-center ml-3 p-2 rounded-3xl border-2 border-solid border-ga-primary
+      ' >#ตัวร้อน</p>
+    </div>
+  )
+}
 
