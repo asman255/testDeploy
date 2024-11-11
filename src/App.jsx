e98c16652370c1815898389352a30062de5a33f6
Phai <phai.339@gmail.com>
2024-11-11 09:46:36 +0700

    updtecardcategory

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import CardCategry from '../src/components/Category/CardCategry'
+import Category from './page/Category'
 
 function App() {
 
 
   return (
     <>
-    <div className="flex flex-wrap pt-2 ">
-    <CardCategry/>
-    <CardCategry/>
-    <CardCategry/>
-    <CardCategry/>
-    <CardCategry/>
-    <CardCategry/>
-    <CardCategry/>
-    <CardCategry/>
-    
+    <div>
+    <Category />
     </div>
    
- 
     </>
   )
 }
