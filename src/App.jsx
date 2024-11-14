e772c180b1eda21fb2e05bfc27555b4b1e6a4d45
LONESOME <pasterbasher@gmail.com>
2024-11-14 11:42:32 +0700

    Home CategoryCard Slide

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Home from './page/Home'
+import Category from './page/Category'
 
 function App() {
   return (
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -10,13 +10,15 @@ const Home = () => {
     <Nav logo search/>
     <div className='my-16 p-3 lg:my-40'>
       <p>หมวดหมู่</p>
+      <div className='flex items-center overflow-x-auto snap-x snap-mandatory -mx-4 space-x-7'>
+      <CardCategry /> <CardCategry /> <CardCategry /> <CardCategry /> <CardCategry />
+      <CardCategry /><CardCategry /><CardCategry /><CardCategry /><CardCategry /><CardCategry />
+      </div>
+      
+     
     </div>
-      <CardCategry />
-    <div className='my-52 p-3 lg:my-40 flex justify-between'>
-      <p>สินค้าแนะนำ</p>
-      <a href='#'>เพิ่มเติม</a> 
-    </div>
-      <CardProduct />
+      
+   
     <NavMobile />
     </>
     
