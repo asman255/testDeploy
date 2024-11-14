1d640ecf465e3c040dc7180c74abd5754c20c220
Phai <phai.339@gmail.com>
2024-11-14 10:13:12 +0700

    update

--- src/page/Category.jsx
+++ src/page/Category.jsx
@@ -2,26 +2,25 @@ import React from "react";
 import CardCategry from "../components/Category/CardCategry";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import NavMobile from "../components/Navbar/NavMobile"
+import Nav from "../components/Navbar/Nav" 
+
 const Category = () => {
   const handleBack = () => {
     window.history.back(); // ฟังก์ชันย้อนกลับไปหน้าก่อนหน้า
   };
   return (
     <>
-      <div className="flex items-center justify-between w-full mt-14 px-6">
-        <div className="cursor-pointer" onClick={handleBack}>
-          <IoMdArrowRoundBack size={25} />
-        </div>
-        <div className=" flex-1 text-center text-2xl ">
-          <h1>หมวดหมู่สินค้า</h1>
-        </div>
-      </div>
-      <div className="flex flex-wrap justify-center mt-10  gap-4">
+    <div>
+      <div className="text-2xl pt-6 ">
+    <Nav  back title ="หมวดหมู่ทั้งหมด" />
+    </div>
+      <div className="  my-16 lg:my-40 flex flex-wrap justify-center mt-10 gap-4">
         <CardCategry />
         <CardCategry />
         <CardCategry />
       </div>
       <NavMobile/>
+      </div>
     </>
   );
 };
