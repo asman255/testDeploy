77282ed9fa060ea6cb10d10aef43d8994bbf8a39
LONESOME <pasterbasher@gmail.com>
2024-11-14 11:11:00 +0700

    pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-
+import Home from './page/Home'
 
 function App() {
 
@@ -8,7 +8,7 @@ function App() {
   return (
 
     <>
-
+    <Home />
     </>
 
   )
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Nav from '../components/Navbar/Nav'
+import NavMobile from '../components/Navbar/NavMobile'
+import CardCategry from '../components/Category/CardCategry'
+import CardProduct from '../components/Product/CardProduct'
 
-const test = () => {
+const Home = () => {
   return (
-    <div>test 000XXXXXXXX</div>
+    <>
+    <Nav logo search/>
+    <div className='my-16 p-3 lg:my-40'>
+      <p>หมวดหมู่</p>
+    </div>
+      <CardCategry />
+    <div className='my-52 p-3 lg:my-40 flex justify-between'>
+      <p>สินค้าแนะนำ</p>
+      <a href='#'>เพิ่มเติม</a> 
+    </div>
+      <CardProduct />
+    <NavMobile />
+    </>
     
   )
 }
 
-export default test
+export default Home
