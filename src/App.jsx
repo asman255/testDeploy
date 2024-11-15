ae9b2475570018af169b7a4c79bb578972734f77
asman255 <smandth@gmail.com>
2024-11-15 23:30:36 +0700

    create CartPage n modified CardCard make it flex for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-<<<<<<< HEAD
-// import OrderHistory from './page/OrderHistory'
-import ProductDetail from './page/ProductDetail'
-import Category from './page/Category'
-function App() {
-  return (
-    <>
-      {/* <OrderHistory /> */}
-      <ProductDetail />
-      {/* <Category /> */}
-=======
 import Home from './page/Home'
 import Category from './page/Category'
-
+import Cart from './page/Cart'
 function App() {
   return (
     <>
-    <Home />
->>>>>>> ae16d16fc23131203184ef35e386f2bfc16ccece
+    <Cart />
     </>
 
     
