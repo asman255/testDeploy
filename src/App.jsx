ee8ad698db0223766150f84fade188bbcf5d5242
asman255 <smandth@gmail.com>
2024-11-16 22:25:02 +0700

    commit for pull

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import Home from './page/Home'
 import Category from './page/Category'
 import Cart from './page/Cart'
+import ProductDetail from './page/ProductDetail'
+import OrderHistory from './page/OrderHistory'
 function App() {
   return (
     <>
