f9675d1198073dbd23b572b85eaeb4d3ae0d2485
Phai <phai.339@gmail.com>
2024-11-16 21:51:42 +0700

    loginupdate

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Home from './page/Home'
 import Category from './page/Category'
+import Login from './page/Login'
 
 function App() {
   return (
     <>
-    <Home />
+    <Login/>
     </>
   );
 }
